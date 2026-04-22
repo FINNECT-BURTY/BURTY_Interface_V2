060ec0d5de9bc354f1a94a1e312b9260d0b90238
RosieOh <dhxogns920@gmail.com>
2026-04-22 21:43:10 +0900

    CHORE: UserRepository.java 코드 반영
    무엇: 애플리케이션 기능 코드 자산 반영
    왜: 도메인 동작 명세 연결 및 확장 기반 확보

---
+++ src/main/java/com/nuri/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nuri.domain.repository;
+
+import com.nuri.domain.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+}
