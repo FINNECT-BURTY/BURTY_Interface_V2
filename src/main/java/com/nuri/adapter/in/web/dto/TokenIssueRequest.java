58f541825fdc0199b47dbe295578252cc7bf87bc
RosieOh <dhxogns920@gmail.com>
2026-04-22 21:43:05 +0900

    CHORE: TokenIssueRequest.java 코드 반영
    무엇: 애플리케이션 기능 코드 자산 반영
    왜: 도메인 동작 명세 연결 및 확장 기반 확보

---
+++ src/main/java/com/nuri/adapter/in/web/dto/TokenIssueRequest.java
@@ -0,0 +1,13 @@
+package com.nuri.adapter.in.web.dto;
+
+public class TokenIssueRequest {
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
