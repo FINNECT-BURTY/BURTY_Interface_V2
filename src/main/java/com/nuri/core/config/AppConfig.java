eeb7e5044ce3e1b993a973ad9ca0e86085e7d403
RosieOh <dhxogns920@gmail.com>
2026-04-22 21:43:08 +0900

    CHORE: AppConfig.java 코드 반영
    무엇: 애플리케이션 기능 코드 자산 반영
    왜: 도메인 동작 명세 연결 및 확장 기반 확보

---
+++ src/main/java/com/nuri/core/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.nuri.core.config;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
