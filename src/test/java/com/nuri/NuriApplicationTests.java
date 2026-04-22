3cdd061f3136ec99de2a5fcfa8f189db4e4b5aba
RosieOh <dhxogns920@gmail.com>
2026-04-22 21:43:11 +0900

    CHORE: NuriApplicationTests.java 테스트 반영
    무엇: 단위·통합 테스트 시나리오 자산 반영
    왜: 회귀 검증 강화 및 품질 안정성 확보

---
+++ src/test/java/com/nuri/NuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
