02dcaac40bdcd8f9e17ec65a1736d05f0fcd087c
RosieOh <dhxogns920@gmail.com>
2026-04-22 21:43:06 +0900

    CHORE: MyDataAssetResponse.java 코드 반영
    무엇: 애플리케이션 기능 코드 자산 반영
    왜: 도메인 동작 명세 연결 및 확장 기반 확보

---
+++ src/main/java/com/nuri/adapter/out/mydata/dto/MyDataAssetResponse.java
@@ -0,0 +1,14 @@
+package com.nuri.adapter.out.mydata.dto;
+
+public class MyDataAssetResponse {
+    private Double totalAsset;
+    private Double monthlySpend;
+    private Double volatilityPercent;
+
+    public Double getTotalAsset() { return totalAsset; }
+    public void setTotalAsset(Double totalAsset) { this.totalAsset = totalAsset; }
+    public Double getMonthlySpend() { return monthlySpend; }
+    public void setMonthlySpend(Double monthlySpend) { this.monthlySpend = monthlySpend; }
+    public Double getVolatilityPercent() { return volatilityPercent; }
+    public void setVolatilityPercent(Double volatilityPercent) { this.volatilityPercent = volatilityPercent; }
+}
