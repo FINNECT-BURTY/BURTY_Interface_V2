d39b96bbd33595fa3871e7e672c25ffdc5e29698
RosieOh <dhxogns920@gmail.com>
2026-05-14 14:10:30 +0900

    CHORE: BertyApplicationTests.java 테스트 반영
    무엇: 테스트 코드 `src/test/java/com/berty/BertyApplicationTests.java` 반영
    왜: 회귀 검증·품질 안전망 및 스펙 고정 효과 확보

---
+++ src/test/java/com/berty/BertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
