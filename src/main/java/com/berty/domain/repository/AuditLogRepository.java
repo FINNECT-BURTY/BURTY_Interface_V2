7ef456d19aa5aaa2d293b99b3e0b61bb7756046a
RosieOh <dhxogns920@gmail.com>
2026-05-14 14:10:25 +0900

    FEAT: AuditLogR 저장소 반영
    무엇: Berty 애플리케이션 코드
    `src/main/java/com/berty/domain/repository/AuditLogRepository.java` 반영
    왜: 도메인 기능·어댑터 계층 확장 및 API 계약 충족

---
+++ src/main/java/com/berty/domain/repository/AuditLogRepository.java
@@ -0,0 +1,7 @@
+package com.berty.domain.repository;
+
+import com.berty.domain.entity.AuditLogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepository extends JpaRepository<AuditLogEntity, Long> {
+}
