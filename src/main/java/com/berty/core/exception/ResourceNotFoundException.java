1a42f91c3ceb17d917e9744ac2f01307201ef275
RosieOh <dhxogns920@gmail.com>
2026-05-14 14:10:22 +0900

    FEAT: ResourceNotFoundException.java 반영
    무엇: Berty 애플리케이션 코드
    `src/main/java/com/berty/core/exception/ResourceNotFoundException.java`
    반영
    왜: 도메인 기능·어댑터 계층 확장 및 API 계약 충족

---
+++ src/main/java/com/berty/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package com.berty.core.exception;
+
+import com.berty.core.error.enums.ErrorCode;
+
+public class ResourceNotFoundException extends BusinessException {
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(ErrorCode.ENTITY_NOT_FOUND,
+                String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+    }
+
+    public ResourceNotFoundException(String message) {
+        super(ErrorCode.ENTITY_NOT_FOUND, message);
+    }
+
+    public ResourceNotFoundException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+}
